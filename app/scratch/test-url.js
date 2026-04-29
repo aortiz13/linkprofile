30a7a325e93dd4ae2c628c5fec5c7313fd9437fd
aortiz13 <serviciosmensualesadrian@gmail.com>
2026-04-29 15:51:16 -0300

    fix: remove app/scratch from git tracking (breaks production build)

--- app/scratch/test-url.js
+++
@@ -1,4 +0,0 @@
-console.log(new URL('//assets.nflxext.com/img.png', 'https://example.com/ar/').href);
-console.log(new URL('/assets/img.png', 'https://example.com/ar/').href);
-console.log(new URL('assets/img.png', 'https://example.com/ar/').href);
-console.log(new URL('https://absolute.com/img.png', 'https://example.com/ar/').href);
